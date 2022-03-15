a19438b51835b96bde7b5e24fd9c50dea123a835
xn <280145668@qq.com>
2022-03-16 00:05:52 +0800

    release 1.0

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
-# lua-resty-
-
+# lua-resty-object
+lua object inspired by javascript object
 # Synopsis
 ```lua
-
+local object = require("resty.object")
+assert(object{a=1,b=2,c=3}:keys():as_set() == array{'a','b','c'}:as_set())
+assert(object{a=1,b=2,c=3}:values():as_set() == array{1,2,3}:as_set())
+assert(object{a=1,b=2} == object{a=1,b=2})
+assert(object{a=1,b={c=3,d=4}} == object{a=1,b={c=3,d=4}})
+assert(object{a=1,b={c=3,d=4}} ~= object{a=1,b={c=3,d=5}})
+assert(object{a=1}:assign({b=2}, {c=3}) == object{a=1,b=2,c=3})
+assert(object.from_entries(object{a=1,b=2}:entries():map(function(e) return {'k'..e[1], 100 + e[2]} end)) == object{ka=101,kb=102})
 ```
-
+# api
+## object.assign(t, ...)
+## object.keys()
+## object.values()
+## object.entries()
+## object.from_entries(t)
+## object.equals(t, o)
+deeply compare if `t` equals `o`.
+## object.contains(t, o)
+deeply compare if `t` contains `o`.
---
+++ resty/object.lua
@@ -0,0 +1,83 @@
+local array = require("resty.array")
+local pairs = pairs
+local ipairs = ipairs
+local select = select
+
+local object = setmetatable({}, {
+  __call = function(t, attrs)
+    return setmetatable(attrs or {}, t)
+  end
+})
+object.__index = object
+function object.new(cls, t)
+  return setmetatable(t or {}, cls)
+end
+function object.assign(t, ...)
+  local n = select("#", ...)
+  for i = 1, n do
+    for k, v in pairs(select(i, ...)) do
+      t[k] = v
+    end
+  end
+  return t
+end
+function object.entries(t)
+  local res = array:new()
+  for k, v in pairs(t) do
+    res[#res + 1] = {k, v}
+  end
+  return res
+end
+function object.from_entries(arr)
+  local res = object:new()
+  for _, e in ipairs(arr) do
+    res[e[1]] = e[2]
+  end
+  return res
+end
+object.fromEntries = object.from_entries
+
+function object.keys(t)
+  local res = array:new()
+  for k, _ in pairs(t) do
+    res[#res + 1] = k
+  end
+  return res
+end
+
+function object.values(t)
+  local res = array:new()
+  for _, v in pairs(t) do
+    res[#res + 1] = v
+  end
+  return res
+end
+
+function object.contains(t, o)
+  for k, v in pairs(o) do
+    if t[k] ~= v and (type(v) ~= 'table' or type(t[k]) ~= 'table' or not object.equals(v, t[k])) then
+      return false
+    end
+  end
+  return true
+end
+
+function object.__eq(t, o)
+  return object.contains(t, o) and object.contains(o, t)
+end
+object.equals = object.__eq
+
+if select('#', ...) == 0 then
+  assert(object {a = 1, b = 2, c = 3}:keys():as_set() == array {'a', 'b', 'c'}:as_set())
+  assert(object {a = 1, b = 2, c = 3}:values():as_set() == array {1, 2, 3}:as_set())
+  assert(object {a = 1, b = 2} == object {a = 1, b = 2})
+  assert(object {a = 1, b = {c = 3, d = 4}} == object {a = 1, b = {c = 3, d = 4}})
+  assert(object {a = 1, b = {c = 3, d = 4}} ~= object {a = 1, b = {c = 3, d = 5}})
+  assert(object {a = 1}:assign({b = 2}, {c = 3}) == object {a = 1, b = 2, c = 3})
+  assert(object.from_entries(object {a = 1, b = 2}:entries():map(function(e)
+    return {'k' .. e[1], 100 + e[2]}
+  end)) == object {ka = 101, kb = 102})
+  print("all tests passed!")
+end
+
+return object
